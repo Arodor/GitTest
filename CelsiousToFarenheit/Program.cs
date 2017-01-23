f4addbee41a30a816b3391868e1b1571926d417d
Arodor <hristobalinov@gmail.com>
2017-01-23 19:25:26 +0200

    Added house price project

---
+++ CelsiousToFarenheit/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CelsiousToFarenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Number of farenheits to be converted: ");
+            var farenheit = double.Parse(Console.ReadLine());
+            var formula = farenheit * 1.8 + 32;
+            Console.Write("Celsius = ");
+            Console.WriteLine(formula);
+
+        }
+    }
+}
---
+++ HousePrice/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HousePrice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var RoomSmall = int.Parse(Console.ReadLine());
+            var Kitchen = int.Parse(Console.ReadLine());
+            var squareArea = Double.Parse(Console.ReadLine());
+            
+            var bathroom = RoomSmall / 2;
+            var  SecondRoom = (RoomSmall * 0.1) + RoomSmall;
+            var thirdRoom = (SecondRoom * 0.1) + SecondRoom;
+
+            var Coridor = (RoomSmall + Kitchen + bathroom + SecondRoom + thirdRoom)*0.05;
+            var Output = (RoomSmall + Kitchen + bathroom + SecondRoom + thirdRoom) + Coridor;
+            var total = (Coridor * Output);
+            Console.WriteLine("{0:F2}",total);
+        }
+    }
+}
