e12bdfa15e788a25f386c1544ea536a235d045c1
Arodor <hristobalinov@gmail.com>
2017-01-23 09:30:15 +0200

    Added new project VeggieMarket

---
+++ VeggieMarket/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeggieMarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                 var PriceKGVeggie = double.Parse(Console.ReadLine());
+
+                  var PriceKGFruit = double.Parse(Console.ReadLine());
+
+                   var TotalVeggieKilo = double.Parse(Console.ReadLine());
+
+                    var TotalFruitKilo = double.Parse(Console.ReadLine());
+                     var euro = 1.94;
+
+            var outputVeg = PriceKGVeggie * TotalVeggieKilo / euro;
+            var outputFruit = PriceKGFruit * TotalFruitKilo / euro;
+            Console.WriteLine(outputVeg + outputFruit);
+         
+        }
+    }
+}
